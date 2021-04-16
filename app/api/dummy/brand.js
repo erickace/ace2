26419aa7674231a926731b4c3ac9ed7e0d449949
erickace <erickfersanchez@gmail.com>
2021-04-15 21:03:19 -0600

    version 1

--- app/api/dummy/brand.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'Ace 2 Pro',
-  desc: 'Ace 2 Pro - React Website Arqui 2',
-  prefix: 'ace',
-  footerText: 'Ace 2 Pro All Rights Reserved 2021',
-  logoText: 'Ace 2 Pro',
-};
--- app/services/apirest.js
+++
@@ -1,1 +0,0 @@
-export const Apiurl = "https://1ggozdi7qb.execute-api.us-east-2.amazonaws.com/etapa_prueba";
